e00283d77e45df90b4c55a185cdd5db64ce741c4
ramonmluz <rmluzz@gmail.com>
2025-07-26 10:35:42 -0300

    Add dependencies

--- MovieHub/app/build.gradle.kts
+++ MovieHub/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -37,12 +38,46 @@ android {
 
 dependencies {
 
+    // AndroidX Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    //Kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.test)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    androidTestImplementation(libs.androidx.navigation.testing) // Geralmente para testes de UI/navegação
+
+    // Networking (Retrofit, OkHttp, Json)
+    implementation(libs.retrofit.core)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.serialization.converter)
+
+    // DI (Koin)
+    implementation(libs.koin.android)
+
+    // Image Loading (Glide)
+    implementation(libs.glide.core)
+    annotationProcessor(libs.glide.compiler) // Se você usa Java
+    // kapt(libs.glide.compiler) // Se você usa Kotlin e o plugin 'kotlin-kapt'
+
+    //Test
     testImplementation(libs.junit)
+    testImplementation( libs.mockk.core)
+    testImplementation( libs.mockk.android)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
