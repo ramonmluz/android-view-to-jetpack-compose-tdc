f830af0f977360faacc6dfb8d28bec85995b0ffb
ramonmluz <rmluzz@gmail.com>
2025-09-02 15:41:07 -0300

    Adapter adjust

--- MovieHub/app/src/main/java/br/com/ramonmluz/moviehub/presentation/ui/adapter/MovieAdapter.kt
+++ MovieHub/app/src/main/java/br/com/ramonmluz/moviehub/presentation/ui/adapter/MovieAdapter.kt
@@ -1,8 +1,6 @@
 package br.com.ramonmluz.moviehub.presentation.ui.adapter
 
 import android.content.Context
-import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,23 +8,17 @@ import br.com.ramonmluz.moviehub.R
 import br.com.ramonmluz.moviehub.data.model.Movie
 import br.com.ramonmluz.moviehub.databinding.ViewMovieItemBinding
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 
 class MovieAdapter(val items: List<Movie>, val context: Context) :
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val viewMovieItemBinding =
             ViewMovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.view_movie_item, parent, false)
         return ViewHolder(viewMovieItemBinding)
     }
 
     override fun getItemCount(): Int {
-        return items.size ?: 0
+        return items.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -53,50 +45,6 @@ class MovieAdapter(val items: List<Movie>, val context: Context) :
             .load(imageUrl)
             .placeholder(R.mipmap.local_movies)
             .error(R.mipmap.ic_launcher)
-            .listener(object : RequestListener<Drawable> { // <-- ADD LISTENER
-//                override fun onLoadFailed(
-//                    e: GlideException?,
-//                    model: Any?,
-//                    target: Target<Drawable>?,
-//                    isFirstResource: Boolean
-//                ): Boolean {
-//                    Log.e("MovieAdapter", "Glide onLoadFailed for URL: $imageUrl", e) // <-- LOG THE EXCEPTION
-//                    // e might be null, but GlideException usually has good details.
-//                    // You can inspect 'e' in the debugger if it's not null.
-//                    return false // MUST return false to allow Glide to set the error drawable
-//                }
-
-                //                override fun onResourceReady(
-//                    resource: Drawable?,
-//                    model: Any,
-//                    target: Target<Drawable>,
-//                    dataSource: DataSource,
-//                    isFirstResource: Boolean
-//                ): Boolean {
-//                    Log.i("MovieAdapter", "Glide onResourceReady for URL: $imageUrl")
-//                    return false // MUST return false to allow Glide to set the loaded image
-//                }
-                override fun onLoadFailed(
-                    e: GlideException?,
-                    model: Any?,
-                    target: Target<Drawable>,
-                    isFirstResource: Boolean
-                ): Boolean {
-                    Log.e("MovieAdapter", "Glide onResourceReady for URL: $imageUrl")
-                    return false // MUST return false to allow Glide to set the loaded image
-                }
-
-                override fun onResourceReady(
-                    resource: Drawable,
-                    model: Any,
-                    target: Target<Drawable>?,
-                    dataSource: DataSource,
-                    isFirstResource: Boolean
-                ): Boolean {
-                    Log.i("MovieAdapter", "Glide onResourceReady for URL: $imageUrl")
-                    return false // MUST return false to allow Glide to set the l
-                }
-            })
             .into(holder.imageView)
     }
 
